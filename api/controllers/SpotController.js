c4e9d4c12b471f5596cf29b1c690592359caba51
Aleksander <aleksander@rendtslev.dk>
2014-09-25 13:58:44 +0200

    added spots

--- api/controllers/SpotController.js
+++ api/controllers/SpotController.js
@@ -6,28 +6,6 @@
  */
 
 module.exports = {
-	addUserToSpot : function(req, res){
-		var userId = req.param('user');
-		var id = req.param('id');
-		var gameId = req.param('gameId');
-		Spot.findOne(id).
-		populate("users").
-		exec(function(err, game){
-			if (err) {
-				return res.serverError(err);
-			}
-			else if(typeof game != 'undefined'){
-				Spot.users.add(userId)
-				Spot.save(function(err, result){
-					Game.publishUpdate(gameId,result);
-				res.json(result);
-				});
-
-			}
-
-
-		});
-
-	}
+	
 };
 
