e5499f1c2fc279592405307c1a99caf150b6ae0f
Aleksander <aleksander@rendtslev.dk>
2014-09-30 13:11:26 +0200

    create game now works

--- assets/app/createGame/controllers/createGameController.js
+++ assets/app/createGame/controllers/createGameController.js
@@ -1,16 +1,16 @@
 angular.module('ubteambuilder.createGame.controllers', []).controller('CreateGameCtrl', CreateGameCtrl);
 
 
-CreateGameCtrl.$inject =  ["GameModel"]
+CreateGameCtrl.$inject =  ["GameModel", "Session", "$location"]
 
-function CreateGameCtrl(){
+function CreateGameCtrl(GameModel, Session, $location){
 	var vm = this;
 	vm.game = {};
 	vm.game.numberOfSpots = 10;
 
 
 	vm.createGame = function(game) {
-		newGame.user = Session.currentUser.id;
+		game.user = Session.currentUser.id;
 		GameModel.create(game).then(function(model) {
 			$location.path("/games/"+model.id);
 		});
