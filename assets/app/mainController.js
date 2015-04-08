ee31eae9f8490080e1017a463a7412720744e7e8
Emil Skovsen Olsen <emilskovsen@gmail.com>
2015-04-08 12:15:28 +0200

    Fixed the Cdn version in the front end to be dynamic

--- assets/app/mainController.js
+++ assets/app/mainController.js
@@ -7,9 +7,9 @@ function MainCtrl($scope, $location, AuthService, Session, ChampionService) {
     Session.currentUser = window.user;
     //Initiate variables
     vm.isAuthorized = AuthService.isAuthorized;
-    // ChampionService.getVersion().then(function(result){
-    //  ChampionService.cdnVersion = result[0];
-    // });
+    ChampionService.getVersion().then(function(result){
+    ChampionService.cdnVersion = result.dd;
+    });
     //Make injections accessible in prototypical functions
     vm.$scope = $scope;
     vm.$location = $location;
